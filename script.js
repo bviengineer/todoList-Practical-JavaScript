ab82d7a0234c30d54247954503136df3794e623b
Kaisma Penn-Titley <kptitley@gmail.com>
2017-11-02 22:59:53 -0400

    todoList app using JavaScript, html which holds buttons to toggle tasks as completed

---
+++ script.js
@@ -0,0 +1,65 @@
+var todoList = {
+  todos: [],
+  displayTodos: function(){
+    if(this.todos.length === 0){
+      console.log("Your todo list is empty");
+    } else {
+        console.log("My Todos:");  
+        for(var i = 0; i < this.todos.length; i++){
+        console.log(this.todos[i].todoText);  
+      }  
+    }
+  },
+  addTodo: function(todoText){
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos(); //this is outside the scope, should we also put others outside the scope they've been declared in?
+  },
+  changeTodo: function(position, todoText){
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position){
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position){  //parenthesis did not show up for me when the toggleComleted command was executed
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function(){
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    
+    //Get number of completed todos
+    for(var i = 0; i < totalTodos; i++){
+      if(this.todos[i].completed === true){
+        completedTodos++
+      }      
+    }
+    
+    //Case 1: If everything is true, make everything false
+    if(completedTodos === totalTodos){
+      for (var i = 0; i < totalTodos; i++){
+        this.todos[i].completed = false;
+      }
+      this.displayTodos();
+    }
+  }
+};
+
+var displayTodosButton = document.getElementById("displayTodosButton");
+var tgogleAllButton = document.getElementById("toggleAllButton");
+
+
+displayTodosButton.addEventListener("click", function() {
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener("click", function() {
+  todoList.toggleAll();
+});
+
