2bd9a6e547ee8ca8392296ad1839cd42578cc5c2
Kaisma Penn-Titley <kptitley@gmail.com>
2017-11-04 11:00:12 -0400

    refactored html code behind buttons and created a js object that holds the functions related to the buttons - commented out the old code

--- js/script.js
+++ js/script.js
@@ -55,15 +55,24 @@ var todoList = {
   }
 };
 
-var displayTodosButton = document.getElementById("displayTodosButton");
-var tgogleAllButton = document.getElementById("toggleAllButton");
+// var displayTodosButton = document.getElementById("displayTodosButton");
+// var tgogleAllButton = document.getElementById("toggleAllButton");
 
 
-displayTodosButton.addEventListener("click", function() {
-  todoList.displayTodos();
-});
+// displayTodosButton.addEventListener("click", function() {
+//   todoList.displayTodos();
+// });
 
-toggleAllButton.addEventListener("click", function() {
-  todoList.toggleAll();
-});
+// toggleAllButton.addEventListener("click", function() {
+//   todoList.toggleAll();
+// });
 
+
+var handlers = {
+  displayTodos: function(){
+    todoList.displayTodos();
+  },
+  toggleAll: function(){
+    todoList.toggleAll();
+  }
+};
